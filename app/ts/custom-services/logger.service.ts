d7cc4543e1d4097beb2a73eb8b1af2225b69fe78
Mister Tandon <enggparveshtandon@gmail.com>
2017-01-20 10:37:00 +0530

    DI with providers: Developed LoggerService used in Dependency Injection with providers demo.

---
+++ app/ts/custom-services/logger.service.ts
@@ -0,0 +1,46 @@
+import {
+    Injectable
+} from '@angular/core';
+
+@Injectable()
+export class LoggerService {
+		
+		/**
+		 *logsArr: Property used to store all the provided logged messaged for current request life cycle.
+		 **/
+    private logsArr: string[] = [];
+
+    constructor() {}
+
+    /**
+     * makeLog: This function is used to create logs. Provided log message pushed into private property.
+     *
+     **/
+    public makeLog(msgInfo: string, isError: Boolean = false): void {
+
+        this.logsArr.push(msgInfo);
+        this.showLog(msgInfo, isError);
+    }
+
+    /**
+     *This function is used to display currently created log.
+     *
+     **/
+    private showLog(msgInfo: string, isError: Boolean): void {
+
+        isError ? console.error(msgInfo) : console.log(msgInfo);
+    }
+
+    /**
+     *getLoggedMessages: It is a getter function, used to get all the logged messages for current request life cycle.
+     *
+     **/
+    get getLoggedMessages() {
+
+        let loggedMessage: string = JSON.stringify(this.logsArr);
+
+        console.log(loggedMessage);
+
+        return loggedMessage;
+    }
+}
--- app/ts/di-with-providers-demo/di-with-providers.component.ts
+++ app/ts/di-with-providers-demo/di-with-providers.component.ts
@@ -1,12 +1,27 @@
 import {
-    Component
+    Component,
+    OnInit
 } from '@angular/core';
+
+import {
+    LoggerService
+} from './../custom-services/logger.service';
 
 @Component({
     selector: "di-with-providers",
     templateUrl: "app/ts/di-with-providers-demo/di-with-providers.component.html",
     styleUrls: ["app/ts/di-with-providers-demo/di-with-providers.component.css"],
+    providers: [LoggerService]
 })
-export class DiWithProvidersComponent {
+export class DiWithProvidersComponent implements OnInit {
 
+    constructor(private Logger_Service_S: LoggerService) {}
+
+    ngOnInit(): void {
+
+        /**
+         *Creating log message using LoggerService, helpfull to display messages in console while this component is created.
+         **/
+        this.Logger_Service_S.makeLog("DiWithProvidersComponent created.");
+    }
 }
